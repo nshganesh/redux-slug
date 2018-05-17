49a988ad92d92ce20f9a03238ac2d52b41e57fe6
Ganesh <ganesh.hegde@ardourlabs.com>
2018-05-17 14:49:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+export const to_slug = (str) => {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  let from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  let to = "aaaaeeeeiiiioooouuuunc------";
+  let i = 0, l = from.length;
+  for ( ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+};
